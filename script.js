f9b2c11ae3d476564d8bad9f236c073dfc8fabdd
Henrique Carvalho <hrq.trabalhos@outlook.com>
2025-01-27 22:04:01 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     var errorMessage = document.getElementById('error-message');
 
     // Dados fictícios para autenticação (apenas para exemplo)
-    var validUsername = "Desbravadores";
-    var validPassword = "2025";
+    var validUsername = "usuario123";
+    var validPassword = "senha123";
 
     // Validação de login (caso insensível a maiúsculas/minúsculas)
     if (username === "" || password === "") {
